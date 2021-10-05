6ea85bcf5d20574c15ce7155546a468bfdb7525d
samshal <samueladeshina73@gmail.com>
2021-10-05 10:55:55 +0100

    aa

--- src/app/field/field.module.ts
+++ src/app/field/field.module.ts
@@ -14,6 +14,7 @@ import { NewIncidenceComponent } from './components/new-incidence/new-incidence.
 import { LeafletModule } from '@asymmetrik/ngx-leaflet';
 import { NgbTypeaheadModule, NgbToastModule } from '@ng-bootstrap/ng-bootstrap';
 
+
 @NgModule({
   declarations: [
     ShellComponent,
